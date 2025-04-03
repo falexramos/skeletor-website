44e73110eafb1b52d067988ebf8778acb8305ee4
favian.ramos <favianalexander.ramos.contractor@bbva.com>
2025-04-03 23:56:53 +0200

    v6

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -6,37 +6,36 @@ export const languageList = {
 export const labels = {
   es: {
     // =========== Navegación =========== //
-    "nav.home": "Inicio",
-    "nav.nosotros": "Quienes somos",
-    "nav.values": "Valores",
-    "nav.projects": "Proyectos",
-    "nav.team": "Equipo",
-    "nav.contact": "Contacto",
-    "nav.action": "Que Hacemos",
-    "nav.donate": "Donaciones",
-    "nav.partnert": "Colaboración",
-    "nav.gallery": "Galería",
-    "nav.text.follow": "Síguenos",
-    // =========== Tema =========== //
-    "theme.toggle": "Cambiar Tema",
+  "nav.home": "Inicio",
+  "nav.nosotros": "Quiénes somos",
+  "nav.values": "Valores",
+  "nav.projects": "Proyectos",
+  "nav.team": "Equipo",
+  "nav.contact": "Contacto",
+  "nav.action": "Qué Hacemos",
+  "nav.donate": "Donaciones",
+  "nav.partnert": "Colaboración",
+  "nav.gallery": "Galería",
+  "nav.text.follow": "Síguenos",
+  // =========== Tema =========== //
+  "theme.toggle": "Cambiar Tema",
 
-    // =========== Footer =========== //
-    "footer.sections.mires.title": "MIRES",
-    "footer.sections.mires.mainContent": "Transformamos la solidaridad en acción, brindando apoyo integral a quienes más lo necesitan.",
+  // =========== Footer =========== //
+  "footer.sections.mires.title": "MIRES",
+  "footer.sections.mires.mainContent": "Transformamos la solidaridad en acción, brindando apoyo integral a quienes más lo necesitan.",
 
-    "footer.sections.projects.title": "Proyectos",
-    "footer.sections.projects.links.dana.text": "DANA",
-    "footer.sections.projects.links.food.text": "Alimentando Esperanza",
-    "footer.sections.projects.links.coschooling.text": "Coschooling",
-    "footer.sections.projects.links.diversamente.text": "Diversamente Hábil",
+  "footer.sections.projects.title": "Proyectos",
+  "footer.sections.projects.links.dana.text": "DANA",
+  "footer.sections.projects.links.food.text": "Alimentando Esperanza",
+  "footer.sections.projects.links.coschooling.text": "Coschooling",
+  "footer.sections.projects.links.diversamente.text": "Diversamente Hábil",
 
+  "footer.sections.contact.title": "Contacto",
+  "footer.sections.contact.address": "Valencia, España",
+  "footer.sections.contact.phone": "+34 722 630 688",
+  "footer.sections.contact.email": "miresvalencia.es@gmail.com",
 
-    "footer.sections.contact.title": "Contacto",
-    "footer.sections.contact.address": "Valencia, España",
-    "footer.sections.contact.phone": "+34 722 630 688",
-    "footer.sections.contact.email": "miresvalencia.es@gmail.com",
-
-    "footer.sections.copy": "MIRES. Todos los derechos reservados.",
+  "footer.sections.copy": "MIRES. Todos los derechos reservados.",
     "footer.sections.desing": "Design by",
   },
   en: {
